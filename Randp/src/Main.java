bc3dc0d7e18ffcba37ec8746d5b09b1b286d7b87
Jerry Huang <jerryh.develop@gmail.com>
2015-12-08 09:24:58 -0800

    Added Randp Project

---
+++ Randp/src/Main.java
@@ -0,0 +1,19 @@
+// Jerry Huang
+// Period 2
+// APCS
+// Randp Project
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Randp rand = new Randp(6);
+		for (int i = 0; i < 9; i++) {
+			System.out.println(rand.nextInt());
+		}
+		
+		
+	}
+
+}
