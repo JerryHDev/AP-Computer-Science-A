9d84cde4204a5197fcb455a02b3e19c4501c705f
Jerry Huang <jerryh.develop@gmail.com>
2016-03-12 13:35:51 -0800

    Added myPoint file

---
+++ My Point/myPoint.java
@@ -0,0 +1,47 @@
+package myPoint;
+
+/*
+ * Jerry Huang
+ * APCS
+ * Period 2
+ * 
+ * 
+ * This class corresponds to an interface that supports a getX, setX, getY, and setY method.
+ */
+public class myPoint {
+	
+	// x and y coordinates for a point
+	private int x;
+	private int y;
+	
+	
+	// Empty constructor
+	public myPoint() {
+		
+	}
+	
+	// Arg-constructor
+	public myPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	/** Getter Methods */
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	
+	
+	/** Setter Methods */
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
